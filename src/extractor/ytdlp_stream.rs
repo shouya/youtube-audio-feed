dd2f966fb3ff1957748f54c75e42ab37809339d6
Shou Ya <shouya@users.noreply.github.com>
2023-08-20 15:38:48 +0900

    allow error on stream

--- src/extractor/ytdlp_stream.rs
+++ src/extractor/ytdlp_stream.rs
@@ -1,9 +1,10 @@
 use async_trait::async_trait;
+use bytes::Bytes;
 use futures::StreamExt;
 use tokio::io::BufReader;
 use tokio_util::io::ReaderStream;
 
-use crate::Result;
+use crate::{Error, Result};
 
 use super::{Extraction, Extractor};
 
@@ -26,17 +27,32 @@ impl Extractor for YtdlpStream {
       .arg("-")
       .arg(url)
       .stdout(std::process::Stdio::piped())
+      .stderr(std::process::Stdio::piped())
       .spawn()?;
 
     let stdout = child.stdout.take().expect("stdout not opened");
-    let stream = ReaderStream::new(BufReader::new(stdout))
+    let stderr = child.stderr.take().expect("stderr not opened");
+
+    let stdout_stream = ReaderStream::new(BufReader::new(stdout))
       .map(|res| res.map_err(|e| e.into()));
-    let stream = stream.boxed();
+    let stderr_stream = ReaderStream::new(BufReader::new(stderr))
+      .map(|res| res.map_err(|e| e.into()))
+      .map(|res| res.and_then(detect_error));
+    let stream = futures::stream::select(stdout_stream, stderr_stream).boxed();
 
     // wait for child to finish in another task
     tokio::spawn(async move { child.wait().await.unwrap() });
 
     let mime_type = String::from("audio/mp4");
     Ok(Extraction::Stream { mime_type, stream })
+  }
+}
+
+fn detect_error(bytes: Bytes) -> Result<Bytes> {
+  let s = String::from_utf8_lossy(&bytes);
+  if s.contains("ERROR:") {
+    Err(Error::AudioStream(s.to_string()))
+  } else {
+    Ok(Bytes::new())
   }
 }
